5383a9e7fe16d6511e37c13831e04813ab292dd0
Raúl Marín <rmarin82@gmail.com>
2020-04-29 13:05:43 +0200

    Register twitter servicer with the DI container.

--- PioPioBlazor/Startup.cs
+++ PioPioBlazor/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using PioPioBlazor.Data;
+using PioPioBlazor.Services;
 
 namespace PioPioBlazor
 {
@@ -22,7 +22,7 @@ namespace PioPioBlazor
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<TwitterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
