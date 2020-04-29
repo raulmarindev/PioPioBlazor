d3b10a818c8595de2d30ba4fbcaa8638aec64cb8
Raúl Marín <rmarin82@gmail.com>
2020-04-29 13:05:18 +0200

    Apply styles and html elements from design.

---
+++ PioPioBlazor/Models/Tweet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PioPioBlazor.Models
+{
+    public class Tweet
+    {
+        public string Text { get; set; } = string.Empty;
+
+        public int? FavoriteCount { get; set; }
+
+        public int? RetweetCount { get; set; }
+
+        public string UserScreenName { get; set; } = string.Empty;
+
+        public string Url { get; set; } = string.Empty;
+
+        public string UserProfileImageUrl { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public string ImageAlt { get; set; } = string.Empty;
+
+        public string UserProfileUrl { get; set; } = string.Empty;
+    }
+}
---
+++ PioPioBlazor/wwwroot/site.js
@@ -0,0 +1,19 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	/************************************************************************/
+/*!**********************!*\
+  !*** ./css/site.css ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack://pio-pio/./css/site.css?");
+/******/ })()
+;
