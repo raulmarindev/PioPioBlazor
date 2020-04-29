41c002aa21bb2a06ac91aebe00e11ac0ddd02b65
Raúl Marín <rmarin82@gmail.com>
2020-04-29 12:54:49 +0200

    Add tailwindcss configuration.

---
+++ PioPioBlazor/webpack.dev.config.js
@@ -0,0 +1,46 @@
+﻿const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        site: './css/site.css',
+    },
+    output: {
+        filename: '[name].js',
+        path: Path.resolve(__dirname, 'wwwroot'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: (loader) => [
+                                require('tailwindcss'),
+                                require('autoprefixer')({}),
+                                require('postcss-import')({ root: loader.resourcePath }),
+                                require('postcss-preset-env')(),
+                            ],
+                            minimize: false
+                        },
+                    }
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: './css/[name].css',
+        }),
+    ],
+};
---
+++ PioPioBlazor/webpack.prod.config.js
@@ -0,0 +1,28 @@
+﻿module.exports = {
+    mode: 'production',
+    entry: { style: './css/site.css' },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: (loader) => [
+                                require('autoprefixer')({}),
+                                require('postcss-import')({ root: loader.resourcePath }),
+                                require('postcss-preset-env')(),
+                                require('cssnano')({
+                                    preset: 'default'
+                                })
+                            ],
+                            minimize: false
+                        },
+                    }
+                ],
+            },
+        ],
+    },
+};
