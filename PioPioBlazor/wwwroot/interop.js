da8284509e242ec977cb275f848e86e151356b4a
Raúl Marín <rmarin82@gmail.com>
2020-04-30 11:48:58 +0200

    Update tailwind css.

---
+++ PioPioBlazor/wwwroot/interop.js
@@ -0,0 +1,19 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	/************************************************************************/
+/*!***********************!*\
+  !*** ./js/interop.js ***!
+  \***********************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+eval("﻿window.tagSelectorComponent = {\r\n    getValue: function (element) {\r\n        return element.value;\r\n    },\r\n    setValue: function (element, value) {\r\n        element.value = value;\r\n    },\r\n    blur: function (element) {\r\n        element.blur();\r\n    }\r\n};\n\n//# sourceURL=webpack://pio-pio/./js/interop.js?");
+/******/ })()
+;
