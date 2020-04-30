98c0115f6632e7dc19f0afa0f9fa60f7002d0927
Raúl Marín <rmarin82@gmail.com>
2020-04-30 19:47:19 +0200

    Add the names of the 30 languages supported by Twitter.

--- PioPioBlazor/Models/Tweet.cs
+++ PioPioBlazor/Models/Tweet.cs
@@ -5,6 +5,49 @@ namespace PioPioBlazor.Models
 {
     public class Tweet
     {
+        private static IDictionary<string, string> _languages;
+
+        static Tweet()
+        {
+            _languages = new Dictionary<string, string>
+            {
+                ["en"] = "English",
+                ["ar"] = "French",
+                ["bn"] = "Bengali",
+                ["cs"] = "Czech",
+                ["da"] = "Danish",
+                ["de"] = "German",
+                ["el"] = "Greek",
+                ["es"] = "Spanish",
+                ["fa"] = "Persian",
+                ["fi"] = "Finnish",
+                ["fil"] = "Filipino",
+                ["fr"] = "French",
+                ["he"] = "Hebrew",
+                ["hi"] = "Hindi",
+                ["hu"] = "Hungarian",
+                ["id"] = "Indonesian",
+                ["it"] = "Italian",
+                ["ja"] = "Japanese",
+                ["ko"] = "Korean",
+                ["msa"] = "Msa",
+                ["nl"] = "Dutch",
+                ["no"] = "Norwegian",
+                ["pl"] = "Polish",
+                ["pt"] = "Portuguese",
+                ["ro"] = "Romanian",
+                ["ru"] = "Russian",
+                ["sv"] = "Swedish",
+                ["th"] = "Thai",
+                ["tr"] = "Turkish",
+                ["uk"] = "Ukranian",
+                ["ur"] = "Urdu",
+                ["vi"] = "Vietnamese",
+                ["zh-cn"] = "Chinese",
+                ["und"] = "Unknown",
+            };
+        }
+
         public string Text { get; set; } = string.Empty;
 
         public int? FavoriteCount { get; set; }
@@ -28,5 +71,16 @@ namespace PioPioBlazor.Models
         public IEnumerable<string> HashTags { get; set; }
 
         public string Language { get; set; }
+
+        public static IDictionary<string, string> Languages
+        {
+            get
+            {
+                return _languages;
+            }
+            private set
+            {
+            }
+        }
     }
 }
