1a2432f2ce510aa534b238b8e9615c6c7e3cd27b
Raúl Marín <rmarin82@gmail.com>
2020-05-01 19:56:02 +0200

    Improve TagSelector so tags can be remove by clicking on them.

--- PioPioBlazor/Components/TagSelectorStyle.cs
+++ PioPioBlazor/Components/TagSelectorStyle.cs
@@ -5,12 +5,12 @@
         public static readonly TagSelectorStyle TailwindCss = new TagSelectorStyle
         {
             SelectorClass = "block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline mb-8",
-            SelectedTagClass = "bg-blue-300 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-full",
+            SelectedTagClass = "bg-blue-300 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-full cursor-pointer",
             DropdownClass = "bg-white shadow rounded-lg py-2 z-10",
             DropdownItemClass = "block px-4 py-2 text-gray-80 hover:bg-indigo-500",
             DropdownItemHoverClass = "bg-indigo-500",
             InputboxClass = "",
-            RemoveTagClass = "fas fa-times"
+            RemoveTagClass = ""
         };
 
         public string SelectorClass { get; set; } = string.Empty;
