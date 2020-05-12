e76439cb6892be014574237f6c008ef1afe517bb
Raúl Marín <rmarin82@gmail.com>
2020-05-12 16:47:13 +0200

    Improve style of the filters inputs.

--- PioPioBlazor/Components/TagSelectorStyle.cs
+++ PioPioBlazor/Components/TagSelectorStyle.cs
@@ -4,7 +4,7 @@
     {
         public static readonly TagSelectorStyle TailwindCss = new TagSelectorStyle
         {
-            SelectorClass = "block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline mb-8",
+            SelectorClass = "block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline mb-8 md:w-5/12",
             SelectedTagClass = "bg-blue-300 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-full cursor-pointer",
             DropdownClass = "bg-white shadow rounded-lg py-2 z-10",
             DropdownItemClass = "block px-4 py-2 text-gray-80 hover:bg-indigo-500",
